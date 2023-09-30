329a0a5a4e7dff11d43d219573a693dd7f086e56
dErnandoV7 <d.ernandov7@gmail.com>
2023-09-30 19:36:08 -0300

    up

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ function add() {
     let boxPartipantes = document.querySelector('.participantes').innerHTML
     document.querySelector('.participantes').innerHTML = boxPartipantes + `<span>${newParticipante.value}</span>`
     arrayParticipantes.push(newParticipante.value)
+  } else {
+    let popUp = document.querySelector('.no-character')
+    popUp.classList.add('popup-animation')
   }
   document.querySelector('#participante').value = ''
 }
@@ -15,11 +18,44 @@ let numberRandom
 
 function sortear() {
   numberRandom = Math.floor(Math.random() * ((arrayParticipantes.length - 1) - 0 + 1)) + 0
+  if (arrayParticipantes.length == 0) {
+    let popUp = document.querySelector('.no-participants')
+    popUp.classList.add('popup-animation')
+  }
 
   for (let i = 0; i < arrayParticipantes.length; i++) {
     if (i == (numberRandom)) {
-      alert(`ganhador ${arrayParticipantes[i]}`)
+      document.querySelector('.vencedor').innerHTML = `<img src="Images/x.svg" onclick="closeDialog()"><p>${arrayParticipantes[i]} foi o vencedor!</p>`
+      console.log(document.querySelector('.vencedor').innerHTML)
+      document.querySelector('.vencedor').showModal()
       break
     }
   }
+}
+
+function closeNoCharacter() {
+  document.querySelector('.no-character').classList.remove('popup-animation')
+}
+
+function closeNoParticipants() {
+  document.querySelector('.no-participants').classList.remove('popup-animation')
+}
+
+function closeDialog() {
+  document.querySelector('.vencedor').close()
+}
+
+function clean() {
+  if (arrayParticipantes.length == 0) {
+    let popUp = document.querySelector('.itIsClean')
+  
+    popUp.classList.add('popup-animation')
+  } else {
+    arrayParticipantes = ''
+    document.querySelector('.participantes').innerHTML = ''
+  }
+}
+
+function closeItIsClean() {
+  document.querySelector('.itIsClean').classList.remove('popup-animation')
 }
